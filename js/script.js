e3ac35fb26e44cf1c4c1d4a08fcd47b9738f27a5
Starkov Vladimir <matmuchrapna@gmail.com>
2012-08-15 03:08:11 +0600

    added logic for basic css

--- js/script.js
+++ js/script.js
@@ -2,25 +2,45 @@ $(function() {
 	
 var precompiled = $('#css_precompiled').html(),
 	aftercompiled = precompiled,
-	customization = $('#css_customization');
+	customization = $('#css_customization'),
+	form = $('#simple-form');
+
+// default values
+var radius = '2px',
+	size = '18px',
+	margin = '5px',
+	bg = '#f1f1f1',
+	shadow = '0 0 3px rgba(0, 0, 0, .35)';
+
+var f_radius = $('input[name="radius"]').val(radius),
+	f_size = $('input[name="size"]').val(size),
+	f_margin = $('input[name="margin"]').val(margin),
+	f_bg = $('input[name="bg"]').val(bg),
+	f_shadow = $('input[name="shadow"]').val(shadow);
 
 
-var radius = '10px',
-	size = '38px',
-	margin = '10px',
-	bg = 'green',
-	shadow = '0 0 3px green';
+function reload_css() {
+	f_radius = $('input[name="radius"]').val(),
+	f_size = $('input[name="size"]').val(),
+	f_margin = $('input[name="margin"]').val(),
+	f_bg = $('input[name="bg"]').val(),
+	f_shadow = $('input[name="shadow"]').val();
 
-setTimeout( function() {
 	aftercompiled = precompiled
-		.replace(/%radius%/g, radius)
-		.replace(/%size%/g, size)
-		.replace(/%margin%/g, margin)
-		.replace(/%bg%/g, bg)
-		.replace(/%shadow%/g, shadow);
-
+		.replace(/%radius%/g, f_radius)
+		.replace(/%size%/g, f_size)
+		.replace(/%margin%/g, f_margin)
+		.replace(/%bg%/g, f_bg)
+		.replace(/%shadow%/g, f_shadow);
 	customization.html( aftercompiled );
-}, 500);
+}
+
+reload_css();
+
+form.on('change keypress keyup blur', 'input', function() {
+	reload_css();
+});
+
 
 
 });
